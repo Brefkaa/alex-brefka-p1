311d6318dbc1d2c92aac2fbdf683d57a5038e8c0
Brefkaa <brefkaa@gmatc.matc.edu>
2024-10-07 17:07:36 -0500

    Completed EF

---
+++ Domain/PackersSchedule.cs
@@ -0,0 +1,13 @@
+namespace Domain;
+
+public class PackersSchedule
+{
+    public int Id { get; set; }
+    public string? Week { get; set; }
+
+    public string? Opponent { get; set; }
+
+    public string? Date { get; set; }
+
+    public string? Time { get; set; }
+}
---
+++ Persistence/Migrations/20241007220142_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PackersSchedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Week = table.Column<string>(type: "TEXT", nullable: true),
+                    Opponent = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<string>(type: "TEXT", nullable: true),
+                    Time = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackersSchedules", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PackersSchedules");
+        }
+    }
+}
